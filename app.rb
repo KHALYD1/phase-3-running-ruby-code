9c2e38bebed6a92b40060ee04ee9ae37c9d82889
Khalid <khaliddakar001@gmail.com>
2023-05-15 12:01:48 +0300

    completed assingment.

--- app.rb
+++ app.rb
@@ -14,10 +14,17 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
 
-puts add(1,3) + add(2,2)
+puts "Hello World!"# Output the desired string
+
+print "Pass this test, please."
+
+p [1,2,3]
+
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
+
+# puts add(1,3) + add(2,2)
